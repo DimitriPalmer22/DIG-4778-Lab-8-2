3fc7028d99e3c575ed524a8553c87013e5e72eb8
lsaenzmarmol <lsaenzmarmol@gmail.com>
2024-10-20 03:13:49 -0400

    Enemy Movement Script Complete

---
+++ Assets/_Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{ 
+    [SerializeField] private float speed = 1;
+    [SerializeField] private int downwardShift = 1;
+
+
+    void Update()
+    {
+        // Have enemy move towards the blocker
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+
+// Once they reach a blocker make them shift one unit down
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Multiply by downwardShift to continue and proceed to the opposite end of screen
+        if(other.gameObject.tag == "Blocker")
+        {
+            transform.position += new Vector3(0, -downwardShift, 0);
+            speed *= -1;    
+        }
+        
+    } 
+}
